913d7436c98b39635095f4b7b4269eaf3d322984
jfoshee <jacobf@gmail.com>
2011-10-28 11:01:28 -0500

    Added NQunit and QUnit-MVC packages.  Added an example test. Enabled deployment for html+js+css files. Changed Interop.SHDocVw.dll reference to { EmbedInteropTypes: False, CopyLocal : True } to avoid a missing reference exception.

---
+++ NQunitExample/ExampleTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NQunitExample
+{
+    [TestClass]
+    public class ExampleTest
+    {
+        [TestMethod,
+        DeploymentItem(@"NQunitExample\ExampleTest.html"),
+        DeploymentItem(@"NQunitExample\Content\qunit.css", @"Content"),
+        DeploymentItem(@"NQunitExample\Scripts\qunit.js", @"Scripts")]
+        public void TestMethod1()
+        {
+            var files = new string[] { Path.Combine(TestContext.DeploymentDirectory, "ExampleTest.html") };
+            var tests = NQUnit.NQUnit.GetTests(30 * 1000, files);
+            foreach (var item in tests)
+            {
+                if (item.InitializationException != null)
+                    throw new Exception("Intialization Exception", item.InitializationException);
+                Assert.AreEqual("pass", item.Result);
+            }
+        }
+
+        public TestContext TestContext { get; set; }
+    }
+}
